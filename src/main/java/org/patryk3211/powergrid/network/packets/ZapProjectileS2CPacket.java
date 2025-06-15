217c8861fe43d1fde2a1ffc3692cd0a79aa82b78
patryk3211 <patrykmierzy@gmail.com>
2025-06-15 15:35:07 +0200

    Zapper entity hit effect.

--- src/main/java/org/patryk3211/powergrid/network/packets/ZapProjectileS2CPacket.java
+++ src/main/java/org/patryk3211/powergrid/network/packets/ZapProjectileS2CPacket.java
@@ -16,26 +16,46 @@
 package org.patryk3211.powergrid.network.packets;
 
 import com.simibubi.create.foundation.networking.SimplePacketBase;
+import it.unimi.dsi.fastutil.ints.IntList;
+import it.unimi.dsi.fastutil.ints.IntLists;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import org.joml.Vector3f;
 import org.patryk3211.powergrid.electricity.particles.SparkParticleData;
+import org.patryk3211.powergrid.electricity.particles.ZapParticleData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ZapProjectileS2CPacket extends SimplePacketBase {
     private enum Type {
-        BLOCK_HIT
+        BLOCK_HIT,
+        ENTITY_HIT
     }
 
     private final Type type;
     private Vector3f pos;
     private Direction dir;
 
+    private int targetEntity;
+    private List<Integer> affectedEntities;
+
     public ZapProjectileS2CPacket(BlockHitResult hit) {
         type = Type.BLOCK_HIT;
         pos = hit.getPos().toVector3f();
         dir = hit.getSide();
+    }
+
+    public ZapProjectileS2CPacket(Entity target, Collection<Entity> affected) {
+        type = Type.ENTITY_HIT;
+        targetEntity = target.getId();
+        affectedEntities = affected.stream().map(Entity::getId).collect(Collectors.toList());
     }
 
     public ZapProjectileS2CPacket(PacketByteBuf buf) {
@@ -44,6 +64,15 @@ public class ZapProjectileS2CPacket extends SimplePacketBase {
             case BLOCK_HIT -> {
                 pos = buf.readVector3f();
                 dir = buf.readEnumConstant(Direction.class);
+            }
+            case ENTITY_HIT -> {
+                targetEntity = buf.readInt();
+
+                int len = buf.readInt();
+                affectedEntities = new ArrayList<>();
+                for(int i = 0; i < len; ++i) {
+                    affectedEntities.add(buf.readInt());
+                }
             }
         }
     }
@@ -56,6 +85,13 @@ public class ZapProjectileS2CPacket extends SimplePacketBase {
                 buf.writeVector3f(pos);
                 buf.writeEnumConstant(dir);
             }
+            case ENTITY_HIT -> {
+                buf.writeInt(targetEntity);
+                buf.writeInt(affectedEntities.size());
+                for(var i : affectedEntities) {
+                    buf.writeInt(i);
+                }
+            }
         }
     }
 
@@ -63,7 +99,31 @@ public class ZapProjectileS2CPacket extends SimplePacketBase {
     public boolean handle(Context context) {
         context.enqueueWork(() -> {
             var world = MinecraftClient.getInstance().world;
-            SparkParticleData.explodeParticles(world, pos.x, pos.y, pos.z, dir, 20);
+            switch(type) {
+                case BLOCK_HIT -> SparkParticleData.explodeParticles(world, pos.x, pos.y, pos.z, dir, 20);
+                case ENTITY_HIT -> {
+                    var target = world.getEntityById(targetEntity);
+                    if(target == null)
+                        return;
+                    var origin = target.getBoundingBox().getCenter();
+                    for(var id : affectedEntities) {
+                        var entity = world.getEntityById(id);
+                        if(entity == null)
+                            continue;
+                        var end = entity.getBoundingBox().getCenter();
+                        world.addImportantParticle(new ZapParticleData((float) end.x, (float) end.y, (float) end.z), true, origin.x, origin.y, origin.z, 0, 0, 0);
+                        var r = world.random;
+                        for(int i = 0; i < 10; ++i) {
+                            float pos = r.nextFloat();
+                            var x = MathHelper.lerp(pos, origin.x, end.x);
+                            var y = MathHelper.lerp(pos, origin.y, end.y);
+                            var z = MathHelper.lerp(pos, origin.z, end.z);
+                            world.addParticle(SparkParticleData.INSTANCE, x, y, z,
+                                    r.nextFloat() - 0.5f, r.nextFloat() - 0.5f, r.nextFloat() - 0.5f);
+                        }
+                    }
+                }
+            }
         });
         return true;
     }
