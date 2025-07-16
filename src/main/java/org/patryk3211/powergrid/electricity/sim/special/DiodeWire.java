1edc85db4d607a882f239f12976cdab4853a8a27
patryk3211 <patrykmierzy@gmail.com>
2025-07-16 14:42:30 +0200

    Made diode much more linear

--- src/main/java/org/patryk3211/powergrid/electricity/sim/special/DiodeWire.java
+++ src/main/java/org/patryk3211/powergrid/electricity/sim/special/DiodeWire.java
@@ -21,27 +21,45 @@ import org.patryk3211.powergrid.electricity.sim.node.IElectricNode;
 import org.patryk3211.powergrid.electricity.sim.solver.ISolverHook;
 
 public class DiodeWire extends AbstractElectricWire implements ISolverHook {
+    private static final float I_LEAK = 1e-6f;
+
     private final float resistance;
     private final float biasVoltage;
     private double prevConductance;
 
+    private double prevCurrent;
+    private double r;
+
     public DiodeWire(float resistance, float biasVoltage, IElectricNode cathode, IElectricNode anode) {
-        super(cathode, anode);
+        super(anode, cathode);
         this.resistance = resistance;
         this.biasVoltage = biasVoltage;
         prevConductance = 0;
+        prevCurrent = 0;
     }
+
+//    private double diodeCurrent(double V) {
+//        var Ilin = V / resistance * 0.5;
+//        var Ia = (Math.tanh((V - biasVoltage) / 0.1) + 1) * Ilin;
+//        return Ia;
+//    }
 
     @Override
     public double conductance() {
-        var anodePotential = node2.getVoltage() - node1.getVoltage();
-        if(anodePotential == 0)
-            return 0;
+        var V = potentialDifference();
+        if(V <= 0)
+            return I_LEAK;
+        if(V >= biasVoltage)
+            return resistance;
+        return (1 / resistance) * V / biasVoltage;
 
-        var V = Math.abs(anodePotential);
-        // Diode equation: I_S * exp(V_a / V_t) Here, V_t is constant at 100mV
-        var Ia = Math.min(Math.exp((anodePotential - biasVoltage) / 0.1f), V / resistance);
-        return Ia / anodePotential;
+//        if(V <= 0)
+//            return I_LEAK;
+
+//        var Ia = diodeCurrent(V);
+//        Ia = Ia * 0.75 + prevCurrent * 0.25;
+//        prevCurrent = Ia;
+//        return Ia / (V + biasVoltage) + I_LEAK;
     }
 
     @Override
@@ -50,4 +68,17 @@ public class DiodeWire extends AbstractElectricWire implements ISolverHook {
         network.updateConductance(this, newConductance - prevConductance);
         prevConductance = newConductance;
     }
+
+//    @Override
+//    public void iteration(DMatrixRMaj A, DMatrixRMaj x, DMatrixRMaj residual, DMatrixRMaj p) {
+//        var V = x.get(node1.getIndex(), 0) - x.get(node2.getIndex(), 0);
+//
+//        var Ia = diodeCurrent(V);
+//        var diff = Ia / (V + biasVoltage) - r;
+//        p.add(node1.getIndex(), 0, -diff);
+//        p.add(node2.getIndex(), 0, diff);
+//        residual.add(node1.getIndex(), 0, -diff);
+//        residual.add(node2.getIndex(), 0, diff);
+//        r += diff;
+//    }
 }
