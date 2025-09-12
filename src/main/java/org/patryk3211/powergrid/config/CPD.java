b9af2075b1ee2ddb17a1f41ce967412391edc68c
Rory <rwjsmithee@aol.com>
2025-09-11 22:29:47 -0400

    Fix some good-nuff values

--- src/main/java/org/patryk3211/powergrid/config/CPD.java
+++ src/main/java/org/patryk3211/powergrid/config/CPD.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 'Rory W. J. Smithee'
+ * Copyright 2025 patryk3211, 'Rory W. J. Smithee'
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,10 @@ package org.patryk3211.powergrid.config;
 import net.createmod.catnip.config.ConfigBase;
 
 public class CPD extends ConfigBase {
-	/* Will you show up or will you just die? */
-	public final ConfigFloat rotorKp = f(0.75f, 0.75f, "rotorKp", Comments.rotorKp);
+	/* Gud Nuff values for the Kp and Kd terms. Tune these to your liking. */
+	public final ConfigFloat rotorKp = f(0.75f, 0f, "rotorKp", Comments.rotorKp);
 	
-	public final ConfigFloat rotorKd = f(10f, 0.25f, "rotorKd", Comments.rotorKd);
+	public final ConfigFloat rotorKd = f(0.2f, 0f, "rotorKd", Comments.rotorKd);
 
     @Override
     public String getName() {
@@ -29,7 +29,8 @@ public class CPD extends ConfigBase {
     }
 
 	private static class Comments {
-				public static final String rotorKp = "Factor to scale the Proportional factor of the Rotor's force calculation";
+		/* bla bla bla bla bla they do the thing. */
+		public static final String rotorKp = "Factor to scale the Proportional factorof the Rotor's force calculation";
 		public static final String rotorKd = "Factor to scale the Differential factor of the Rotor's force calculation";
 	}
 }
