0c66ae557c918c1cc1f75e1144f6b1bcc15eaecf
patryk3211 <patrykmierzy@gmail.com>
2025-09-17 18:11:41 +0200

    Added cold cathode tube to simulation

---
+++ src/main/java/org/patryk3211/powergrid/electricity/sim/special/ColdCathodeTubeWire.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025 patryk3211
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.patryk3211.powergrid.electricity.sim.special;
+
+import org.ejml.data.DMatrixRMaj;
+import org.patryk3211.powergrid.electricity.sim.ElectricalNetwork;
+import org.patryk3211.powergrid.electricity.sim.node.IElectricNode;
+
+public class ColdCathodeTubeWire extends DynamicConductanceWire {
+    private final float breakdownVoltage;
+    private final float holdingVoltage;
+    private final float holdingCurrent;
+    private final float dischargeConductance;
+
+    public boolean lit = false;
+    private double I;
+
+    public ColdCathodeTubeWire(float breakdownVoltage, float holdingVoltage, float holdingCurrent, float dischargeConductance, IElectricNode node1, IElectricNode node2) {
+        super(node1, node2);
+        this.breakdownVoltage = breakdownVoltage;
+        this.holdingVoltage = holdingVoltage;
+        this.holdingCurrent = holdingCurrent;
+        this.dischargeConductance = dischargeConductance;
+    }
+
+    @Override
+    protected double calculateConductance() {
+        if(!lit) {
+            this.I = 0;
+            return ElectricalNetwork.G_MIN;
+        }
+
+        this.I = holdingVoltage * dischargeConductance;
+        return dischargeConductance;
+    }
+
+    @Override
+    public void addResidual(DMatrixRMaj residual) {
+        residual.add(node1.getIndex(), 0, -I);
+        residual.add(node2.getIndex(), 0,  I);
+    }
+
+    @Override
+    public float current() {
+        return (float) (super.current() - I);
+    }
+
+    @Override
+    public float power() {
+        return current() * potentialDifference();
+    }
+
+    @Override
+    public void postUpperSolve() {
+        // Update discharge state
+        if(!lit && potentialDifference() > breakdownVoltage) {
+            lit = true;
+        } else if(lit && current() < holdingCurrent) {
+            lit = false;
+        }
+    }
+}
---
+++ src/test/java/org/patryk3211/electricity/ColdCathodeTubeTests.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2025 patryk3211
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.patryk3211.electricity;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.patryk3211.powergrid.electricity.sim.special.ColdCathodeTubeWire;
+
+public class ColdCathodeTubeTests extends TestHelper {
+    public static void main(String[] args) {
+        var Net = new Network();
+
+        var V1 = Net.V(120f);
+        var GND = Net.V(0);
+
+        var Anode = Net.N();
+        var Cathode = Net.N();
+
+        var Tube = new ColdCathodeTubeWire(90, 60, 0.001f, 0.001f, Anode, Cathode);
+        Net.network.addWire(Tube);
+
+        final var R = 0.001f;
+        Net.W(33000f, V1, Anode);
+        Net.W(R, GND, Cathode);
+        Net.W(100000, Anode, Cathode);
+
+        for(int i = 0; i < 10; ++i) {
+            if(i == 5)
+                V1.setVoltage(180f);
+            Net.calculate();
+
+            System.out.printf("Iteration %d:\n", i);
+            System.out.printf("Anode voltage: %f\n", Anode.getVoltage());
+            System.out.printf("Cathode voltage: %f\n", Cathode.getVoltage());
+            System.out.printf("Tube Conductance: %f\n", Tube.conductance());
+            System.out.printf("Tube Current: %f\n", Tube.current());
+
+            System.out.printf("V1 current: %f\n", V1.getCurrent());
+            System.out.printf("GND current %f\n\n", GND.getCurrent());
+        }
+    }
+
+    @Test
+    void testBelowBreakdown() {
+        var Net = new Network();
+
+        var V1 = Net.V(80);
+        var GND = Net.V(0);
+
+        var Anode = Net.N();
+        var Cathode = Net.N();
+
+        var Tube = new ColdCathodeTubeWire(90, 60, 0.001f, 0.001f, Anode, Cathode);
+        Net.network.addWire(Tube);
+
+        final var R = 0.001f;
+        Net.W(R, V1, Anode);
+        Net.W(R, GND, Cathode);
+
+        for(int i = 0; i < 3; ++i)
+            Net.calculate();
+
+        Assertions.assertEquals(80, Anode.getVoltage(), 1e-6, "Anode voltage is not correct");
+        Assertions.assertEquals(0, V1.getCurrent(), 1e-6, "Source current is not correct");
+        Assertions.assertEquals(V1.getCurrent(), Tube.current(), 1e-6, "Source current is not correct");
+    }
+
+    @Test
+    void testBreakdown() {
+        var Net = new Network();
+
+        var V1 = Net.V(120);
+        var GND = Net.V(0);
+
+        var Anode = Net.N();
+        var Cathode = Net.N();
+
+        var Tube = new ColdCathodeTubeWire(90, 60, 0.001f, 0.001f, Anode, Cathode);
+        Net.network.addWire(Tube);
+
+        final var R = 0.001f;
+        Net.W(33000, V1, Anode);
+        Net.W(R, GND, Cathode);
+
+        for(int i = 0; i < 3; ++i)
+            Net.calculate();
+
+        Assertions.assertEquals(62, Anode.getVoltage(), 0.5, "Anode voltage is not correct");
+        Assertions.assertEquals(V1.getCurrent(), Tube.current(), 1e-6, "Tube current is not correct");
+    }
+}
