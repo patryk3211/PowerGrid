a90583112b71717d94111e1f030b9f563c5e604e
patryk3211 <patrykmierzy@gmail.com>
2025-10-02 11:06:48 +0200

    Optimizer tests

--- src/test/java/org/patryk3211/electricity/OptimizerTests.java
+++ src/test/java/org/patryk3211/electricity/OptimizerTests.java
@@ -15,12 +15,17 @@
  */
 package org.patryk3211.electricity;
 
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.patryk3211.powergrid.electricity.sim.ElectricWire;
 import org.patryk3211.powergrid.electricity.sim.node.FloatingNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OptimizerTests extends TestHelper {
-    private FloatingNode[] buildGrid(Network Net, int size) {
+    private FloatingNode[] buildGrid(Network Net, int size, @Nullable List<ElectricWire> wiresOut) {
         var nodes = new FloatingNode[size * size];
         for(int i = 0; i < size * size; ++i) {
             nodes[i] = Net.N();
@@ -32,12 +37,16 @@ public class OptimizerTests extends TestHelper {
                 var origin = nodes[x + y * size];
                 if(x < size - 1) {
                     // Horizontal
-                    Net.W(r, origin, nodes[x + 1 + y * size]);
+                    var wire = Net.W(r, origin, nodes[x + 1 + y * size]);
+                    if(wiresOut != null)
+                        wiresOut.add(wire);
                     r += 1.0f;
                 }
                 if(y < size - 1) {
                     // Vertical
-                    Net.W(r, origin, nodes[x + (y + 1) * size]);
+                    var wire = Net.W(r, origin, nodes[x + (y + 1) * size]);
+                    if(wiresOut != null)
+                        wiresOut.add(wire);
                     r += 1.0f;
                 }
             }
@@ -52,7 +61,7 @@ public class OptimizerTests extends TestHelper {
 
         // Simulate a NxN grid of nodes connected by resistors
         final int SIZE = 4;
-        var nodes = buildGrid(Net, SIZE);
+        var nodes = buildGrid(Net, SIZE, null);
 
         // Connect sources at corners
         var V1 = Net.V(5);
@@ -92,7 +101,7 @@ public class OptimizerTests extends TestHelper {
         // Simulate a NxN grid of nodes connected by resistors,
         // this time with optimization.
         var Net2 = new Network();
-        var nodes2 = buildGrid(Net2, SIZE);
+        var nodes2 = buildGrid(Net2, SIZE, null);
 
         // Connect sources at corners
         var V2 = Net2.V(5);
@@ -137,5 +146,71 @@ public class OptimizerTests extends TestHelper {
         for(int i = 0; i < SIZE * SIZE; ++i) {
             Assertions.assertEquals(nodes[i].getVoltage(), nodes2[i].getVoltage(), 1e-6, "Optimized network solves with different results");
         }
+    }
+
+    @Test
+    void optimizedStructureChangesTest() {
+        var Net = new Network();
+
+        final int SIZE = 4;
+        var wires = new ArrayList<ElectricWire>();
+        var nodes = buildGrid(Net, SIZE, wires);
+
+        var V1 = Net.V(5);
+        var GND = Net.V(0);
+        Net.W(0.1f, V1, nodes[0]);
+        Net.W(0.1f, GND, nodes[SIZE * SIZE - 1]);
+
+        Net.calculate();
+        wires.get(0).setResistance(10);
+        wires.get(3).setResistance(10);
+        wires.get(9).setResistance(20);
+        wires.get(10).setResistance(50);
+        Net.calculate();
+
+        var Net2 = new Network();
+
+        var wires2 = new ArrayList<ElectricWire>();
+        var nodes2 = buildGrid(Net2, SIZE, wires2);
+
+        var V2 = Net2.V(5);
+        var GND2 = Net2.V(0);
+        Net2.W(0.1f, V2, nodes2[0]);
+        Net2.W(0.1f, GND2, nodes2[SIZE * SIZE - 1]);
+
+        Net2.calculate();
+        wires2.get(0).setResistance(10);
+        wires2.get(3).setResistance(10);
+        wires2.get(9).setResistance(20);
+        wires2.get(10).setResistance(50);
+        Net2.calculate();
+
+        for(int i = 0; i < SIZE * SIZE; ++i) {
+            Assertions.assertEquals(nodes[i].getVoltage(), nodes2[i].getVoltage(), 1e-6, "Optimized network solves with different results");
+        }
+    }
+
+    @Test
+    void optimizedTransformerCoupledNodeTest() {
+        var Net = new Network();
+
+        var V = Net.V(5);
+        var GND = Net.V(0);
+        var S1 = Net.N();
+        var S2 = Net.N();
+        var Tie = Net.N();
+
+        var TR = Net.TR(1, 1, V, GND, S1, S2);
+        Net.W(2, S1, Tie);
+        Net.W(2, S2, Tie);
+
+        Net.network.optimizeNode(Tie);
+        Net.network.optimizeNode(S1);
+        Net.calculate();
+
+        Assertions.assertEquals(1, V.getCurrent(), 1e-6, "Voltage source current is incorrect");
+        Assertions.assertEquals(2.5, Tie.getVoltage(), 1e-6, "Tie node voltage is incorrect");
+        Assertions.assertEquals(4.5, S1.getVoltage(), 1e-6, "Secondary 1 node voltage is incorrect");
+        Assertions.assertEquals(0.5, S2.getVoltage(), 1e-6, "Secondary 2 node voltage is incorrect");
     }
 }
