55146d0d73c6eab775c40b414088eb388a91f44c
patryk3211 <patrykmierzy@gmail.com>
2025-10-29 17:39:40 +0100

    Minimum voltage display for electromagnet

--- src/main/java/org/patryk3211/powergrid/electricity/electromagnet/ElectromagnetBlock.java
+++ src/main/java/org/patryk3211/powergrid/electricity/electromagnet/ElectromagnetBlock.java
@@ -34,6 +34,7 @@ import org.patryk3211.powergrid.electricity.deviceconnector.IAcceptConnector;
 import org.patryk3211.powergrid.electricity.info.IHaveElectricProperties;
 import org.patryk3211.powergrid.electricity.info.Power;
 import org.patryk3211.powergrid.electricity.info.Resistance;
+import org.patryk3211.powergrid.electricity.info.Voltage;
 
 import java.util.List;
 
@@ -65,6 +66,8 @@ public class ElectromagnetBlock extends ElectricBlock implements IBE<Electromagn
     @Override
     public void appendProperties(ItemStack stack, Player player, List<Component> tooltip) {
         Resistance.series(resistance(), player, tooltip);
+        var minCurrent = 0.25f / 0.1f;
+        Voltage.min(minCurrent * resistance(), player, tooltip);
         Power.max(stack, player, tooltip);
     }
 
