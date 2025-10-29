fd3d1646009230a7f293ff5346c1058096b81258
patryk3211 <patrykmierzy@gmail.com>
2025-10-29 11:04:31 +0100

    Clean-up generator coupling code

--- src/main/java/org/patryk3211/powergrid/electricity/sim/special/GeneratorCoupling.java
+++ src/main/java/org/patryk3211/powergrid/electricity/sim/special/GeneratorCoupling.java
@@ -13,8 +13,6 @@ public class GeneratorCoupling extends VoltageSourceCoupling {
     private float baseResistance;
     private float backEmf;
 
-    private double V;
-
     public GeneratorCoupling(IElectricNode positive, @Nullable IElectricNode negative, Number resistance, IRotor rotor) {
         super(positive, negative, resistance);
         this.rotor = rotor;
@@ -23,23 +21,15 @@ public class GeneratorCoupling extends VoltageSourceCoupling {
 
     public void setField(float field) {
         this.field = field;
-        backEmf = field * field * DT / rotor.getInertia(); //2 * (5) / 0.05f;//field * field * DT / rotor.getInertia();
+        backEmf = field * field * DT / rotor.getInertia();
         super.setResistance(baseResistance + backEmf);
     }
 
     @Override
     public void addStaticResidual(IResidualAdder residual) {
         super.addStaticResidual(residual);
-
-        var V_Inductor = (getCurrent() * backEmf + V);// * backEmf / (backEmf + baseResistance);
-//        var G_I = 1 / backEmf;
-
-//        var residualScale = 1 - G_I / (1 / baseResistance + G_I);
-        V = -backEmf * getCurrent();// - V_Inductor;// - V_Inductor) * 1.00001f;
-        // We only want this to oppose, not make the output stronger
-//        if(Math.signum(V) != Math.signum(getVoltage()))
-//            V *= 0.5f;
-        residual.add(index, V);
+        var v = -backEmf * getCurrent();
+        residual.add(index, v);
     }
 
     @Override
