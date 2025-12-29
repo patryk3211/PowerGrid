c0868f9d2c9da23a4d80971d0ffe14433b0f086a
patryk3211 <patrykmierzy@gmail.com>
2025-12-30 00:28:59 +0100

    Add OpenBLAS

--- native/src/powergridNative/include/blas.h
+++ native/src/powergridNative/include/blas.h
@@ -1,14 +1,12 @@
 #pragma once
 
-#include "f2c.h"
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
 // These methods are defined by CBLAS but aren't available through the definitions header.
-int dscal_(integer *n, doublereal *da, doublereal *dx, integer *incx);
-doublereal dasum_(integer *n, doublereal *dx, integer *incx);
+double dasum_(int *n, double *dx, int *incx);
 
 #if defined(__cplusplus)
 }
