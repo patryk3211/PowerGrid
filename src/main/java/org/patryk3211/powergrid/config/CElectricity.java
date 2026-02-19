5be6cda53b9738297e2a4d4393fdd9a4800e40a3
patryk3211 <patrykmierzy@gmail.com>
2026-02-19 11:59:22 +0100

    Make transformer core parameters configurable

--- src/main/java/org/patryk3211/powergrid/config/CElectricity.java
+++ src/main/java/org/patryk3211/powergrid/config/CElectricity.java
@@ -54,6 +54,11 @@ public class CElectricity extends ConfigBase {
     public final ConfigFloat multimeterVoltage = f(500, 1, "multimeterVoltage", Comments.multimeterVoltage);
     public final ConfigFloat multimeterCurrent = f(50, 1, "multimeterCurrent", Comments.multimeterCurrent);
 
+    public final ConfigFloat smallCoreAl = f(1.5f, 0, "smallCoreAl", Comments.smallCoreAl);
+    public final ConfigFloat smallCoreK = f(0.9999f, 0, 1, "smallCoreK", Comments.smallCoreK);
+    public final ConfigFloat mediumCoreAl = f(10.0f, 0, "mediumCoreAl", Comments.mediumCoreAl);
+    public final ConfigFloat mediumCoreK = f(0.99999f, 0, 1, "mediumCoreK", Comments.mediumCoreK);
+
     public final CSolver solver = nested(1, CSolver::new, Comments.solver);
     public final CResistance resistance = nested(1, CResistance::new, Comments.resistance);
     public final CThermal thermal = nested(1, CThermal::new, Comments.thermal);
@@ -105,5 +110,10 @@ public class CElectricity extends ConfigBase {
         public static final String multimeterDistance = "Max multimeter distance";
         public static final String multimeterVoltage = "Max multimeter voltage";
         public static final String multimeterCurrent = "Max multimeter current";
+
+        public static final String smallCoreAl = "Al parameter of small transformer core (affects resistance per turn)";
+        public static final String smallCoreK = "K parameter of small transformer core (affects stray resistance)";
+        public static final String mediumCoreAl = "Al parameter of medium transformer core (affects resistance per turn)";
+        public static final String mediumCoreK = "K parameter of medium transformer core (affects stray resistance)";
     }
 }
