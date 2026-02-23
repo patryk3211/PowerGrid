5ab93c3d8169d15da16fb2bbedabc4734f689b0e
patryk3211 <patrykmierzy@gmail.com>
2026-02-23 17:48:10 +0100

    Force static linking of everything on Windows [skip ci]

--- src/main/java/org/patryk3211/powergrid/electricity/sim/solver/NativeMNA.java
+++ src/main/java/org/patryk3211/powergrid/electricity/sim/solver/NativeMNA.java
@@ -31,7 +31,7 @@ import java.nio.file.Paths;
 import static org.patryk3211.powergrid.electricity.sim.ElectricalNetwork.LOGGER;
 
 public class NativeMNA implements IMNA {
-    private static final String EXPECTED_VERSION = "2";
+    private static final String EXPECTED_VERSION = "3";
 
     private static final int OPERATION_BUFFER_COMMAND_LENGTH = 256;
     private static final int JACOBIAN_COMMAND_SIZE = Integer.BYTES * 2 + Double.BYTES;
