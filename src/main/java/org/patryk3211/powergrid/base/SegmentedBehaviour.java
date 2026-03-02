d03964e2710fdbd87a6498ae8da378cea86e1c98
patryk3211 <patrykmierzy@gmail.com>
2026-03-02 17:37:44 +0100

    Added anti recursion check to segmented behaviour

--- src/main/java/org/patryk3211/powergrid/base/SegmentedBehaviour.java
+++ src/main/java/org/patryk3211/powergrid/base/SegmentedBehaviour.java
@@ -41,6 +41,8 @@ public abstract class SegmentedBehaviour<T extends SegmentedBehaviour<T>> extend
     protected Predicate<T> countToSize;
     private boolean rebuildClient = true;
 
+    private boolean isChecking = false;
+
     public SegmentedBehaviour(SmartBlockEntity be, int maxSize, Predicate<T> countToSize) {
         super(be);
 
@@ -213,6 +215,11 @@ public abstract class SegmentedBehaviour<T extends SegmentedBehaviour<T>> extend
     }
 
     public void checkConnectivity(@Nullable T without) {
+        if(isChecking) {
+            PowerGrid.LOGGER.error("checkConnectivity recursive call prevented");
+            return;
+        }
+        isChecking = true;
         // Make sure this is always run on the controller
         if(!isController() && !getPos().equals(controllerPos)) {
             var controller = getController();
@@ -221,10 +228,13 @@ public abstract class SegmentedBehaviour<T extends SegmentedBehaviour<T>> extend
             } else {
                 PowerGrid.LOGGER.warn("Tried to check connectivity but controller is null");
             }
+            isChecking = false;
             return;
         }
-        if(segments == null)
+        if(segments == null) {
+            isChecking = false;
             return;
+        }
         var allConnected = new HashSet<T>();
         var toCheck = new ArrayList<T>();
         toCheck.add((T) this);
@@ -288,6 +298,7 @@ public abstract class SegmentedBehaviour<T extends SegmentedBehaviour<T>> extend
         }
 
         sync();
+        isChecking = false;
     }
 
     public void remove() {
