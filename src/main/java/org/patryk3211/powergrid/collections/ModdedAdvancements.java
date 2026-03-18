f82abc1f8797282fb8e252927b7903a0348e4ed7
patryk3211 <patrykmierzy@gmail.com>
2026-03-18 12:04:01 +0100

    Write out the advancements

--- src/main/java/org/patryk3211/powergrid/collections/ModdedAdvancements.java
+++ src/main/java/org/patryk3211/powergrid/collections/ModdedAdvancements.java
@@ -6,6 +6,7 @@ import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 import org.patryk3211.powergrid.advancements.PowerGridAdvancement;
 
@@ -18,8 +19,99 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.UnaryOperator;
 
+import static org.patryk3211.powergrid.advancements.PowerGridAdvancement.TaskType.*;
+
 public class ModdedAdvancements implements DataProvider {
     public static final List<PowerGridAdvancement> ENTRIES = new ArrayList<>();
+    public static final PowerGridAdvancement
+
+    ROOT = create("root", b -> b
+            .icon(ModdedBlocks.ELECTRIC_MOTOR)
+            .title("Welcome to Power Grid")
+            .description("Don't electrocute yourself")
+            .special(SILENT)
+            .awardedForFree()),
+
+    /* -------======= Crafting =======------- */
+    ELECTRICAL_AGE = create("conductive_casing", b -> b
+            .icon(ModdedBlocks.CONDUCTIVE_CASING)
+            .title("The Electrical Age")
+            .description("Apply Zinc Ingot to Andesite Casing, creating a casing for your electrical machines")
+            .whenIconCollected()
+            .after(ROOT)
+            .special(NOISY)),
+    ELECTRONIC_WONDERS = create("electronic_wonders", b -> b
+            .icon(ModdedItems.ELECTRICAL_GIZMO)
+            .title("Electronic wonders")
+            .description("Obtain an Electrical Gizmo")
+            .special(NOISY)
+            .after(ELECTRICAL_AGE)
+            .whenIconCollected()),
+    MAGIC_METAL = create("magic_metal", b -> b
+            .icon(ModdedItems.MAGNET)
+            .title("Magic metal")
+            .description("Obtain a Magnet")
+            .special(NOISY)
+            .after(ELECTRICAL_AGE)
+            .whenIconCollected()),
+    ZAPPER = create("zapper", b -> b
+            .icon(ModdedItems.ELECTROZAPPER)
+            .title("I cast lightning bolt! ...gun")
+            .description("Obtain an Electron-Zapper")
+            .after(ELECTRONIC_WONDERS)
+            .whenIconCollected()),
+    ACID = create("acid", b -> b
+            .icon(ModdedFluids.acid().getBucket())
+            .title("Evil water")
+            .description("Make acid")
+            .after(ROOT)
+            .whenIconCollected()),
+
+    /* -------======= Custom Triggers =======------- */
+    BLOW_UP = create("blow_up", b -> b
+            .icon(Items.TNT)
+            .title("My mango, is to blow up")
+            .description("Make something explode (be more careful next time)")
+            .after(ELECTRICAL_AGE)),
+    LIGHTNING = create("lightning", b -> b
+            .icon(Items.LIGHTNING_ROD)
+            .title("Poking grey clouds")
+            .description("Trigger a thunder strike with a thunder attractor")
+            .after(ROOT)),
+    ELECTRIC_MOTOR = create("electric_motor", b -> b
+            .icon(ModdedBlocks.ELECTRIC_MOTOR)
+            .title("Stress through wires")
+            .description("Place and activate an Electrical Motor")
+            .after(ELECTRICAL_AGE)),
+    BOOSTING_CHIP = create("boosting_chip", b -> b
+            .icon(ModdedItems.INTEGRATED_CIRCUIT)
+            .title("To another level")
+            .description("Use a boosting chip")
+            .after(ELECTRONIC_WONDERS)),
+    TRANSFORMER = create("transformer", b -> b
+            .icon(ModdedBlocks.TRANSFORMER_CORE)
+            .title("AC/DC wannabe")
+            .description("Assemble and power a transformer")
+            .after(ELECTRICAL_AGE)),
+    GAUGE = create("gauge", b -> b
+            .icon(ModdedBlocks.VOLTAGE_METER)
+            .title("Monitoring the electrons")
+            .description("Use an electric gauge")
+            .after(ELECTRICAL_AGE)),
+
+    /* -------======= Ooo, very ~secret~, don't look here =======------- */
+    INNOVATIVE_KITCHEN = create("innovative_kitchen", b -> b
+            .icon(Items.BAKED_POTATO)
+            .title("Innovative kitchen")
+            .description("Cook a Potato Battery")
+            .special(SECRET)),
+    WRONG_TOOL = create("wrong_tool", b -> b
+            .icon(Items.SHEARS)
+            .title("Wrong tool for the job")
+            .description("Use shears to cut a wire")
+            .special(SECRET))
+
+    ;
 
     private static PowerGridAdvancement create(String id, UnaryOperator<PowerGridAdvancement.Builder> b) {
         return new PowerGridAdvancement(id, b);
