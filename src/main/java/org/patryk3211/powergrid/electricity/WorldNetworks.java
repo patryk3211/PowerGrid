5b68ebb43c0a17ae70ed64d8ee439460baab9e32
patryk3211 <patrykmierzy@gmail.com>
2026-03-25 15:48:40 +0100

    Use the newly migrated node instead of the old node

--- src/main/java/org/patryk3211/powergrid/electricity/WorldNetworks.java
+++ src/main/java/org/patryk3211/powergrid/electricity/WorldNetworks.java
@@ -513,10 +513,12 @@ public class WorldNetworks extends SavedData implements NetworkGraph.IGraphModif
         var nNode1 = endpoint1.getNode(world);
         if(node1 != nNode1) {
             addAndMigrateNode(node1, nNode1);
+            node1 = nNode1;
         }
         var nNode2 = endpoint2.getNode(world);
         if(node2 != nNode2) {
             addAndMigrateNode(node2, nNode2);
+            node2 = nNode2;
         }
 
         add(endpoint1);
