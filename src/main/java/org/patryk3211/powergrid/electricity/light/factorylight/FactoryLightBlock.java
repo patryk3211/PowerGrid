ba6abd96f0d1ba1eb5689f0158c96bbd50cdb843
rvndm <vm23573468@gmail.com>
2026-03-29 04:26:00 -0300

    Revert "made the factory light look good, it displays well on your hand and connects properly on all directions, updated the texture for the factory lights along with the godrays and recoded the way they connect sideways + did forge datagen (sorry for amount of files i couldn't guarantee i didn't miss any important ones)"
    This reverts commit 4f9b23df484257115102a564880521fb31f5fbe9.

--- src/main/java/org/patryk3211/powergrid/electricity/light/factorylight/FactoryLightBlock.java
+++ src/main/java/org/patryk3211/powergrid/electricity/light/factorylight/FactoryLightBlock.java
@@ -18,11 +18,11 @@ import org.patryk3211.powergrid.electricity.deviceconnector.IAcceptConnector;
 import org.patryk3211.powergrid.electricity.wire.powercord.IAcceptCord;
 
 public class FactoryLightBlock extends HorizontalAxisElectricBlock implements IAcceptCord, IAcceptConnector, IBE<FactoryLightBlockEntity> {
-    public static final IntegerProperty PART = IntegerProperty.create("part", 0, 6);
+    public static final IntegerProperty PART = IntegerProperty.create("part", 0, 3);
     public static final IntegerProperty POWER = IntegerProperty.create("power", 0, 3);
 
     public FactoryLightBlock(Properties settings) {
-        super(settings.noOcclusion().lightLevel(state -> switch(state.getValue(POWER)) {
+        super(settings.lightLevel(state -> switch(state.getValue(POWER)) {
             case 2 -> 10;
             case 3 -> 15;
             default -> 0;
@@ -37,37 +37,27 @@ public class FactoryLightBlock extends HorizontalAxisElectricBlock implements IA
 
     private static boolean canConnect(Direction dir, BlockState neighbour) {
         int part = neighbour.getValue(PART);
-        var axis = neighbour.getValue(HORIZONTAL_AXIS);
-
-        if (part == 0) {
+        if(part == 0) {
+            // Singular piece can always be extended
             return true;
         }
-
-        // Determine valid range based on axis
-        boolean isZ = axis == Direction.Axis.Z;
-        boolean isX = axis == Direction.Axis.X;
-
-        if (isZ && (part < 1 || part > 3)) return false;
-        if (isX && (part < 4 || part > 6)) return false;
-
-        // Center pieces cannot be extended
-        if ((isZ && part == 2) || (isX && part == 5)) {
+        if(part == 2) {
+            // Cannot connect third lamp into a central piece
             return false;
         }
-
-        if (axis != dir.getAxis()) {
+        if(neighbour.getValue(HORIZONTAL_AXIS) != dir.getAxis()) {
+            // Wrong axis, cannot extend
             return false;
         }
-
-        // Edge blocking logic
-        if ((part == 1 || part == 4) && dir.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
+        if(part == 1 && dir.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
+            // Broken state?
             return false;
         }
-
-        if ((part == 3 || part == 6) && dir.getAxisDirection() == Direction.AxisDirection.NEGATIVE) {
+        if(part == 3 && dir.getAxisDirection() == Direction.AxisDirection.NEGATIVE) {
+            // Broken state?
             return false;
         }
-
+        // Remaining states should be fine to extend.
         return true;
     }
 
@@ -75,108 +65,53 @@ public class FactoryLightBlock extends HorizontalAxisElectricBlock implements IA
     public @Nullable BlockState getStateForPlacement(BlockPlaceContext ctx) {
         var dir = ctx.getClickedFace();
         var clickedState = ctx.getLevel().getBlockState(ctx.getClickedPos().relative(dir, -1));
-
-        if (clickedState.is(this) && dir.getAxis() != Direction.Axis.Y) {
-            if (canConnect(dir, clickedState)) {
-
-                int part;
-                if (dir.getAxis() == Direction.Axis.Z) {
-                    part = dir.getAxisDirection() == Direction.AxisDirection.POSITIVE ? 3 : 1;
-                } else {
-                    part = dir.getAxisDirection() == Direction.AxisDirection.POSITIVE ? 6 : 4;
-                }
-
+        if(clickedState.is(this) && dir.getAxis() != Direction.Axis.Y) {
+            if(canConnect(dir, clickedState)) {
                 return defaultBlockState()
                         .setValue(HORIZONTAL_AXIS, dir.getAxis())
-                        .setValue(PART, part);
+                        .setValue(PART, dir.getAxisDirection() == Direction.AxisDirection.POSITIVE ? 3 : 1);
             }
         }
-
         return super.getStateForPlacement(ctx);
-    }
-    private static boolean isCenter(int part) {
-        return part == 2 || part == 5;
-    }
-
-    private static boolean isNegativeEdge(int part) {
-        return part == 1 || part == 4;
-    }
-
-    private static boolean isPositiveEdge(int part) {
-        return part == 3 || part == 6;
-    }
-
-    private static int getCenter(Direction.Axis axis) {
-        return axis == Direction.Axis.Z ? 2 : 5;
-    }
-
-    private static int getNegativeEdge(Direction.Axis axis) {
-        return axis == Direction.Axis.Z ? 1 : 4;
-    }
-
-    private static int getPositiveEdge(Direction.Axis axis) {
-        return axis == Direction.Axis.Z ? 3 : 6;
     }
 
     @Override
-    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState,
-                                  LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
-
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
         int part = state.getValue(PART);
-
-        if (neighborState.is(this)) {
-            if (neighborState.getValue(HORIZONTAL_AXIS) == direction.getAxis()) {
-
+        if(neighborState.is(this)) {
+            if(neighborState.getValue(HORIZONTAL_AXIS) == direction.getAxis()) {
                 int nPart = neighborState.getValue(PART);
-                var axis = direction.getAxis();
                 var axisDir = direction.getAxisDirection();
-
-                if (part == 0 && axisDir == Direction.AxisDirection.POSITIVE &&
-                        (isPositiveEdge(nPart) || isCenter(nPart))) {
-
-                    return state.setValue(HORIZONTAL_AXIS, axis)
-                            .setValue(PART, getNegativeEdge(axis));
-
-                } else if (part == 0 && axisDir == Direction.AxisDirection.NEGATIVE &&
-                        (isNegativeEdge(nPart) || isCenter(nPart))) {
-
-                    return state.setValue(HORIZONTAL_AXIS, axis)
-                            .setValue(PART, getPositiveEdge(axis));
-
-                } else if (isNegativeEdge(part) && axisDir == Direction.AxisDirection.NEGATIVE &&
-                        (isNegativeEdge(nPart) || isCenter(nPart))) {
-
-                    return state.setValue(PART, getCenter(axis));
-
-                } else if (isPositiveEdge(part) && axisDir == Direction.AxisDirection.POSITIVE &&
-                        (isPositiveEdge(nPart) || isCenter(nPart))) {
-
-                    return state.setValue(PART, getCenter(axis));
+                if(part == 0 && axisDir == Direction.AxisDirection.POSITIVE && (nPart == 3 || nPart == 2)) {
+                    return state
+                            .setValue(HORIZONTAL_AXIS, direction.getAxis())
+                            .setValue(PART, 1);
+                } else if(part == 0 && axisDir == Direction.AxisDirection.NEGATIVE && (nPart == 1 || nPart == 2)) {
+                    return state
+                            .setValue(HORIZONTAL_AXIS, direction.getAxis())
+                            .setValue(PART, 3);
+                } else if(part == 1 && axisDir == Direction.AxisDirection.NEGATIVE && (nPart == 1 || nPart == 2)) {
+                    return state.setValue(PART, 2);
+                } else if(part == 3 && axisDir == Direction.AxisDirection.POSITIVE && (nPart == 3 || nPart == 2)) {
+                    return state.setValue(PART, 2);
                 }
             }
-
-        } else if (neighborState.is(Blocks.AIR)) {
-
+        } else if(neighborState.is(Blocks.AIR)) {
             var axis = state.getValue(HORIZONTAL_AXIS);
-
-            if (axis == direction.getAxis()) {
-
-                if (isNegativeEdge(part) && direction.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
+            if(axis == direction.getAxis()) {
+                if(part == 1 && direction.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
                     return state.setValue(PART, 0);
-
-                } else if (isPositiveEdge(part) && direction.getAxisDirection() == Direction.AxisDirection.NEGATIVE) {
+                } else if(part == 3 && direction.getAxisDirection() == Direction.AxisDirection.NEGATIVE) {
                     return state.setValue(PART, 0);
-
-                } else if (isCenter(part)) {
-                    if (direction.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
-                        return state.setValue(PART, getPositiveEdge(axis));
+                } else if(part == 2) {
+                    if(direction.getAxisDirection() == Direction.AxisDirection.POSITIVE) {
+                        return state.setValue(PART, 3);
                     } else {
-                        return state.setValue(PART, getNegativeEdge(axis));
+                        return state.setValue(PART, 1);
                     }
                 }
             }
         }
-
         return super.updateShape(state, direction, neighborState, level, pos, neighborPos);
     }
 
