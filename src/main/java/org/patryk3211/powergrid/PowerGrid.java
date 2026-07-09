14e8f49c2323ca6bb2c4bf09f54b741bc13f39b8
durgon <alex.neitzel@live.com>
2026-07-09 14:57:25 -0700

    Post merge fix

--- src/main/java/org/patryk3211/powergrid/PowerGrid.java
+++ src/main/java/org/patryk3211/powergrid/PowerGrid.java
@@ -104,7 +104,6 @@ public class PowerGrid {
 	private static void setup() {
 		RedstoneConverterRegistry.init();
 		ModdedAdvancements.register();
-		ModdedContraptions.register();
 	}
 
 	private static void playerQuit(ServerPlayer player) {
