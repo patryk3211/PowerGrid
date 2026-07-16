84480f0d3fc2f924eae6b3ac9d0348446ae88686
patryk3211 <patrykmierzy@gmail.com>
2026-07-16 22:08:09 +0200

    Tweak daylight sensor compat

--- src/main/java/org/patryk3211/powergrid/electricity/redstoneconverter/DaylightSensorBehaviour.java
+++ src/main/java/org/patryk3211/powergrid/electricity/redstoneconverter/DaylightSensorBehaviour.java
@@ -2,26 +2,26 @@ package org.patryk3211.powergrid.electricity.redstoneconverter;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.DaylightDetectorBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class DaylightSensorBehaviour implements IRedstoneConverterBehaviour {
     @Override
     public float getSignal(Level level, BlockState state, BlockPos pos, Direction face) {
-        float levelLight = level.getBrightness(LightLayer.SKY, pos) - level.getSkyDarken();
-        float sunAngle = level.getSunAngle(1.0F);
+        // 0.25 - 0.75 = Night
+        float timeOfDay = level.getTimeOfDay(1.0f);
         boolean invert = state.getValue(DaylightDetectorBlock.INVERTED);
         if (invert) {
-            levelLight = 15 - levelLight;
-        } else if (levelLight > 0) {
-            float g = sunAngle < (float)Math.PI ? 0.0F : ((float)Math.PI * 2F);
-            sunAngle += (g - sunAngle) * 0.2F;
-            levelLight = levelLight * Mth.cos(sunAngle);
+            timeOfDay -= 0.25f;
+            return 1 - Math.abs(timeOfDay - 0.25f) / 0.25f;
+        } else {
+            if(timeOfDay > 0.75f) {
+                timeOfDay -= 0.75f;
+            } else {
+                timeOfDay += 0.25f;
+            }
+            return 1 - Math.abs(timeOfDay - 0.25f) / 0.25f;
         }
-
-        return Mth.clamp(levelLight / 15f, 0, 1);
     }
 }
