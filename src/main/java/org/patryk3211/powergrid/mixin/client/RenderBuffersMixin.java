eaea43a69ebb22209cb9948bf08f6f6c4de94907
patryk3211 <patrykmierzy@gmail.com>
2026-07-20 18:04:11 +0200

    mixin fix

--- src/main/java/org/patryk3211/powergrid/mixin/client/RenderBuffersMixin.java
+++ src/main/java/org/patryk3211/powergrid/mixin/client/RenderBuffersMixin.java
@@ -2,12 +2,9 @@ package org.patryk3211.powergrid.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.ByteBufferBuilder;
-import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import net.minecraft.client.renderer.RenderBuffers;
-import net.minecraft.client.renderer.RenderType;
 import org.patryk3211.powergrid.collections.ModdedRenderLayers;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,15 +12,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import java.util.SequencedMap;
 
 @Mixin(RenderBuffers.class)
-public abstract class RenderBuffersAccessor {
-    @Shadow
-    private static void put(Object2ObjectLinkedOpenHashMap<RenderType, ByteBufferBuilder> mapBuilders, RenderType renderType) {
-    }
-
-    @Inject(method="Lnet/minecraft/client/renderer/RenderBuffers;<init>",
+public abstract class RenderBuffersMixin {
+    @Inject(method = "<init>",
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/client/renderer/MultiBufferSource;immediateWithBuffers(Ljava/util/SequencedMap;Lcom/mojang/blaze3d/vertex/ByteBufferBuilder;)Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;"))
-    void getBufferSource(int bufferCount, CallbackInfo ci, @Local(name = "sequencedMap") SequencedMap<RenderType, ByteBufferBuilder> sequencedMap) {
+    void powerGrid$addLayer(int bufferCount, CallbackInfo ci, @Local SequencedMap sequencedMap) {
         var layer = ModdedRenderLayers.getAdditive();
         sequencedMap.put(layer, new ByteBufferBuilder(layer.bufferSize()));
     }
