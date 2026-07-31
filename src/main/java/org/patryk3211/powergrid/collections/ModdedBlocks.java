c8d945af728874f0e89aabbf54128ada80839add
patryk3211 <patrykmierzy@gmail.com>
2026-07-31 18:40:07 +0200

    Basic slab and stair CTs

--- src/main/java/org/patryk3211/powergrid/collections/ModdedBlocks.java
+++ src/main/java/org/patryk3211/powergrid/collections/ModdedBlocks.java
@@ -45,6 +45,9 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.StairBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Half;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.block.state.properties.StairsShape;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
@@ -60,10 +63,7 @@ import org.patryk3211.powergrid.config.CStress;
 import org.patryk3211.powergrid.config.CThermal;
 import org.patryk3211.powergrid.electricity.basinheater.BasinHeaterBlock;
 import org.patryk3211.powergrid.electricity.basinheater.BasinHeaterBlockEntity;
-import org.patryk3211.powergrid.electricity.battery.BatteryBlock;
-import org.patryk3211.powergrid.electricity.battery.BatteryCTBehaviour;
-import org.patryk3211.powergrid.electricity.battery.PotatoBatteryBlock;
-import org.patryk3211.powergrid.electricity.battery.SimpleBatterySpec;
+import org.patryk3211.powergrid.electricity.battery.*;
 import org.patryk3211.powergrid.electricity.bell.AlarmBellBlock;
 import org.patryk3211.powergrid.electricity.carbonpile.CarbonPileBlock;
 import org.patryk3211.powergrid.electricity.carbonpile.CarbonPileCoilBlock;
@@ -146,6 +146,9 @@ public class ModdedBlocks {
             .transform(CThermal.maxPower(100, 1.5f))
             .transform(DisplaySource.displaySource(ModdedDisplaySources.BATTERY))
             .onRegister(CreateRegistrate.connectedTextures(BatteryCTBehaviour::new))
+            .item(BatteryItem::new)
+                .model((ctx, prov) -> prov.blockItem(ctx))
+                .build()
             .register();
 
     public static final BlockEntry<PotatoBatteryBlock> POTATO_BATTERY = REGISTRATE.block("potato_battery", PotatoBatteryBlock::new)
@@ -189,8 +192,25 @@ public class ModdedBlocks {
             .register();
 
     public static final BlockEntry<StairBlock> COPPER_PLATING_STAIRS = REGISTRATE.block("copper_plating_stairs", p -> new StairBlock(COPPER_PLATING.getDefaultState(), p))
-            .blockstate((ctx, prov) -> prov.stairsBlock(ctx.get(), prov.modLoc("block/solar_panel/copper_plating")))
+            .blockstate((ctx, prov) -> prov.stairsBlock(ctx.get(), prov.modLoc("block/copper_plating")))
             .initialProperties(SharedProperties::softMetal)
+            .onRegister(connectedTextures(() -> new EncasedCTBehaviour(ModdedPartialModels.COPPER_PLATING)))
+            .onRegister(casingConnectivity((block, cc) -> cc.make(block, ModdedPartialModels.COPPER_PLATING, (s, f) -> {
+                var shape = s.getValue(StairBlock.SHAPE);
+                var half = s.getValue(StairBlock.HALF);
+                var facing = s.getValue(StairBlock.FACING);
+                if(half == Half.TOP && f == Direction.UP)
+                    return true;
+                if(half == Half.BOTTOM && f == Direction.DOWN)
+                    return true;
+                if(facing == f)
+                    return true;
+                if(shape == StairsShape.INNER_LEFT && f == facing.getCounterClockWise())
+                    return true;
+                if(shape == StairsShape.INNER_RIGHT && f == facing.getClockWise())
+                    return true;
+                return false;
+            })))
             .recipe((ctx, prov) -> prov
                     .stonecutting(DataIngredient.items(COPPER_PLATING.get()), RecipeCategory.DECORATIONS, ctx::get, 1))
             .simpleItem()
@@ -200,8 +220,17 @@ public class ModdedBlocks {
             .blockstate((ctx, prov) ->
                     prov.slabBlock(ctx.get(),
                             prov.modLoc("block/copper_plating"),
-                            prov.modLoc("block/solar_panel/copper_plating")))
+                            prov.modLoc("block/copper_plating")))
             .initialProperties(SharedProperties::softMetal)
+            .onRegister(connectedTextures(() -> new EncasedCTBehaviour(ModdedPartialModels.COPPER_PLATING)))
+            .onRegister(casingConnectivity((block, cc) -> cc.make(block, ModdedPartialModels.COPPER_PLATING, (s, f) -> {
+                var type = s.getValue(SlabBlock.TYPE);
+                if(type == SlabType.TOP && f == Direction.UP)
+                    return true;
+                if(type == SlabType.BOTTOM && f == Direction.DOWN)
+                    return true;
+                return false;
+            })))
             .recipe((ctx, prov) -> prov
                     .stonecutting(DataIngredient.items(COPPER_PLATING.get()), RecipeCategory.DECORATIONS, ctx::get, 2))
             .simpleItem()
