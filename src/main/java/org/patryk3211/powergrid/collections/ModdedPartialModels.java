910492b6ed6a8050bf20d5c73e2818b33dca538f
mechspace2 <61834471+mechspace2@users.noreply.github.com>
2026-08-20 07:13:15 +0200

    Register thyratron glow

--- src/main/java/org/patryk3211/powergrid/collections/ModdedPartialModels.java
+++ src/main/java/org/patryk3211/powergrid/collections/ModdedPartialModels.java
@@ -44,6 +44,7 @@ public class ModdedPartialModels {
 
     public static final PartialModel ELECTRON_TUBE_GLOW = model("component/triode_glow");
     public static final PartialModel PENTODE_GLOW = model("component/pentode_glow");
+    public static final PartialModel THYRATRON_GLOW = model("component/thyratron_glow");
     public static final PartialModel REGULATOR_TUBE_GLOW = model("component/regulator_tube_glow");
     public static final PartialModel POTENTIOMETER_KNOB = model("component/potentiometer_knob");
 
