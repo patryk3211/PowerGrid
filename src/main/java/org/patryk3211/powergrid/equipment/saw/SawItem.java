cac9b01c10c5f5fa1f84e644f1056176dff0e406
patryk3211 <patrykmierzy@gmail.com>
2026-08-24 12:16:31 +0200

    Make the saw cut trees well

--- src/main/java/org/patryk3211/powergrid/equipment/saw/SawItem.java
+++ src/main/java/org/patryk3211/powergrid/equipment/saw/SawItem.java
@@ -1,9 +1,12 @@
 package org.patryk3211.powergrid.equipment.saw;
 
+import com.simibubi.create.content.kinetics.saw.TreeCutter;
+import com.simibubi.create.foundation.utility.AbstractBlockBreakQueue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.ItemStack;
@@ -17,6 +20,7 @@ import org.patryk3211.powergrid.equipment.ItemBoostUtils;
 import org.patryk3211.powergrid.equipment.portablebattery.BatteryUtils;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SawItem extends AxeItem {
     public SawItem(Properties properties) {
@@ -51,6 +55,10 @@ public class SawItem extends AxeItem {
         return (isCorrectToolForDrops(state) ? baseSpeed() : 1.0F) * (ItemBoostUtils.isBoosted(stack) ? 2 : 1);
     }
 
+    private static void dropTreeItem(Level level, BlockPos pos, ItemStack stack) {
+        level.addFreshEntity(new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack.copy()));
+    }
+
     @Override
     public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity miningEntity) {
         if(miningEntity instanceof Player player) {
@@ -58,6 +66,14 @@ public class SawItem extends AxeItem {
             float power = BatteryUtils.drawEnergy(player, energyPerUse() * (boosted ? 2 : 1));
             if(!level.isClientSide && state.getDestroySpeed(level, pos) != 0.0F && power == 0) {
                 stack.hurtAndBreak(1, miningEntity, (livingEntity) -> livingEntity.broadcastBreakEvent(EquipmentSlot.MAINHAND));
+            }
+            if(power >= 0.3f && !level.isClientSide) {
+                Optional<AbstractBlockBreakQueue> dynamicTree = TreeCutter.findDynamicTree(state.getBlock(), pos);
+                if (dynamicTree.isPresent()) {
+                    dynamicTree.get().destroyBlocks(level, null, (pos1, stack1) -> dropTreeItem(level, pos1, stack1));
+                } else {
+                    TreeCutter.findTree(level, pos, state).destroyBlocks(level, null, (pos1, stack1) -> dropTreeItem(level, pos1, stack1));
+                }
             }
             return true;
         }
