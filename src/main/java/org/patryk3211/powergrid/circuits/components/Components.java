0a06db54a30d7c10343d444efba20d054b50ec60
JordanGrant3D <jordangrant3d@gmail.com>
2026-08-28 11:38:01 +0200

    Upgraded pcb visual in editor

--- src/main/java/org/patryk3211/powergrid/circuits/components/Components.java
+++ src/main/java/org/patryk3211/powergrid/circuits/components/Components.java
@@ -115,9 +115,9 @@ public class Components {
             .register();
 
     public static final RegistryEntry<SwitchComponent> SWITCH = REGISTRATE.component("switch", SwitchComponent::new)
-            .footprint(4, 3, b -> b
+            .footprint(3, 3, b -> b
                     .addPad(0, 1, 0)
-                    .addPad(2, 1, 1)
+                    .addPad(2, 0, 1)
                     .addPad(2, 2, 2)
                     .withItem().withOutline())
             .item(ModdedBlocks.LV_SWITCH)
@@ -215,7 +215,7 @@ public class Components {
     public static final RegistryEntry<ButtonComponent> BUTTON = REGISTRATE.component("button", ButtonComponent::new)
             .footprint(3, 3, b -> b
                     .addPad(0, 1, 0)
-                    .addPad(2, 1, 1)
+                    .addPad(2, 0, 1)
                     .addPad(2, 2, 2)
                     .withItem().withOutline()
             )
